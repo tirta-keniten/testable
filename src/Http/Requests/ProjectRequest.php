e59b494d7ddcd3152bf394f9ff2cf679f1b8bfd7
Tirta Keniten <astadala.hospitality@gmail.com>
2020-03-19 17:15:08 +0800

    Project request with validations

---
+++ src/Http/Requests/ProjectRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Testable\Http\Requests;
+
+use Auth;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:191',
+            // 'description' => 
+        ];
+    }
+}
