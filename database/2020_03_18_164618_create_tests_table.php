7c49d936183fca279a41f5953f977ebba39d36dd
Tirta Keniten <astadala.hospitality@gmail.com>
2020-03-19 09:07:33 +0800

    Add softdeletes in test migration

--- database/2020_03_18_164618_create_tests_table.php
+++ database/2020_03_18_164618_create_tests_table.php
@@ -23,6 +23,7 @@ class CreateTestsTable extends Migration
             $table->text('initial_location')->nullable();
             $table->text('prerequiresite')->nullable();
             $table->boolean('status');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
